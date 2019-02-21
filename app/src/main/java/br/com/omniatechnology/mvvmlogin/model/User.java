8afbfdc68006af0210049074a759b1adc82be4ad
legionario07 <paulinho.legionario@hotmail.com>
2019-02-21 18:07:11 -0300

    Parcelable User

--- app/src/main/java/br/com/omniatechnology/mvvmlogin/model/User.java
+++ app/src/main/java/br/com/omniatechnology/mvvmlogin/model/User.java
@@ -1,11 +1,13 @@
 package br.com.omniatechnology.mvvmlogin.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
 import com.google.gson.annotations.SerializedName;
 
-public class User {
+public class User implements Parcelable {
 
     @NonNull
     @SerializedName("usuario")
@@ -43,5 +45,36 @@ public class User {
 
     public boolean isInputDataValid() {
         return !TextUtils.isEmpty(getUsuario()) && getSenha().length() > 2;
+    }
+
+
+
+
+    protected User(Parcel in) {
+        this.mUsuario = in.readString();
+        this.mSenha = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mUsuario);
+        dest.writeString(this.mSenha);
     }
 }
