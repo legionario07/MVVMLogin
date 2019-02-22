1310aacbf59711796c25b4b4427ef29e30bad25e
legionario07 <paulinho.legionario@hotmail.com>
2019-02-22 14:16:24 -0300

    Clear ao error no Login

--- app/src/main/java/br/com/omniatechnology/mvvmlogin/MainActivity.java
+++ app/src/main/java/br/com/omniatechnology/mvvmlogin/MainActivity.java
@@ -5,6 +5,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import br.com.omniatechnology.mvvmlogin.databinding.ActivityMainBinding;
@@ -13,14 +14,18 @@ import br.com.omniatechnology.mvvmlogin.viewmodel.LoginViewModel;
 
 public class MainActivity extends AppCompatActivity implements IView {
 
+    private ActivityMainBinding activityMainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setViewModel(new LoginViewModel(this));
         activityMainBinding.executePendingBindings();
+
+
 
     }
 
@@ -34,10 +39,13 @@ public class MainActivity extends AppCompatActivity implements IView {
     @Override
     public void onSuccess(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void onError(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        activityMainBinding.inEmail.getText().clear();
+        activityMainBinding.inPassword.getText().clear();
     }
 }
